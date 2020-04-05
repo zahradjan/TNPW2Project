a42055606480d22d5f5300f8b08a07a058d656c3
ocasusMaximus <za.jan@seznam.cz>
2020-04-05 23:13:17 +0200

    Books create index new

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
+const booksRouter = require('./routes/books')
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended: false}))
 
 const mongoose = require('mongoose')
 
@@ -23,5 +27,7 @@ db.on('error', error => console.error(error))
 db.on('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
+app.use('/books', booksRouter)
 
 app.listen(process.env.PORT || 3000)
